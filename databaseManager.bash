2d996fc3aba7c4b5a827ebc1dc5544ad2c782845
alexania <alexanianaram@gmail.com>
2023-12-08 00:51:19 -0500

    Create and delete databases

--- databaseManager.bash
+++ databaseManager.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ./databasePass
+
 echo "Welcome to the Database manager."
 
 user=$(whoami)
@@ -20,8 +22,21 @@ while [[ 1 ]];do
 		"Create_Database")
 			read -p "Enter the name of your database: " name
 			echo "Creating Database"
-			mysql -u $user -e "create database if not exists $name"
+			mysql -u $user --password="$dataPass" -e "create database if not exists $name"
 			echo "Database created"
+		;;
+		"Delete_Database")
+			query="show databases"
+			databases=$(mysql -u $user --password="$dataPass" -s -N -e "$query" | grep -v 'information_schema' | grep -v 'performance_schema' | grep -v 'sys' | grep -v 'mysql')
+			num=1
+			for database in $databases; do
+				echo "$num $database"
+				((num++))
+			done
+			read -p "Which database would you like to delete: " dataNum
+			database=$(echo $databases | awk -v var=$dataNum '{print $var}')
+			mysql -u $user --password="$dataPass" -e "drop database $database"
+			echo "Database Removed"
 		;;
 	esac
 done
